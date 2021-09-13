9f00fecead64c1b1b955737996240519fac4824a
eskerthebreton <mranasilver@yahoo.com>
2021-09-13 17:36:08 -0400

    Generalized charge prone to allow other animals

--- conjure-animals.R
+++ conjure-animals.R
@@ -1,38 +1,59 @@
 library(tidyverse)
-
 source("utils.R")
 
-pack_tactics_damage <- function(AC, atk, dpth, n_animals, n_pack_tactics)
+pack_tactics_damage <- function(AC, atk, dpth, dptc, n_animals, n_pack_tactics)
 {
   ## AC:   enemy AC
   ## atk:  animal attack bonus
   ## dpth: expected damage per turn if all attacks hit
+  ## dptc: expected damage per turn if all attacks crit  
   ## n_animals: number of animals
   ## n_pack_tactics: number assumed to get pack tactics (should be <= n_animals, obviously)
-  hit_prob     = hit_chance(atk, AC)
-  hit_prob_adv = hit_chance_adv(atk, AC)
-  dpr          = n_pack_tactics * hit_prob_adv * dpth + 
-                  (n_animals - n_pack_tactics) * hit_prob * dpth
+  hit_prob      = hit_chance(atk, AC)
+  hit_prob_adv  = hit_chance_adv(atk, AC)
+  crit_prob     = 0.05
+  crit_prob_adv = 1 - (1 - 0.05)^2
+  dpr           = n_pack_tactics * (hit_prob_adv * dpth + crit_prob_adv * dptc) +
+                  (n_animals - n_pack_tactics) * (hit_prob * dpth + crit_prob * dptc)
   return(dpr)
 }
 
-elk_damage <- function(AC, STRsave, n_elks, n_charges)
+charge_prone_damage <- function(
+  AC, 
+  STRsave, 
+  n_animals, 
+  n_charges, 
+  prone_DC, 
+  atk_mod,
+  charge_dmg_dice,
+  non_charge_dmg_dice,
+  dmg_mod)
 {
-  atk = 5
-  DC  = 13
-  charge_damage     = 3 * 3.5 + 3
-  non_charge_damage = 2 * 2.5 + 3
+  atk = atk_mod
+  DC  = prone_DC
+  charge_damage          = charge_dmg_dice + dmg_mod
+  charge_crit_damage     = charge_dmg_dice
+  non_charge_damage      = non_charge_dmg_dice + dmg_mod
+  non_charge_crit_damage = non_charge_dmg_dice
   hit_prob     = hit_chance(atk, AC)
   hit_prob_adv = hit_chance_adv(atk, AC)
+  crit_prob     = 0.05
+  crit_prob_adv = 1 - (1 - 0.05)^2
   prone_chance = 1 - hit_chance(STRsave, DC)
   hit_and_prone = hit_prob * prone_chance
-  cumulative_charge_attempts = c(0:min(n_charges, n_elks - 1))
-  if(n_charges < n_elks - 1) {
-    cumulative_charge_attempts = c(cumulative_charge_attempts, rep(n_charges, n_elks-1-n_charges))
+  cumulative_charge_attempts = c(0:min(n_charges, n_animals - 1))
+  if(n_charges < n_animals - 1) {
+    cumulative_charge_attempts = c(cumulative_charge_attempts, rep(n_charges, n_animals-1-n_charges))
   }
-  still_standing  = (1 - hit_and_prone)^cumulative_charge_attempts
-  to_hit_sequence = (1 - still_standing) * hit_prob_adv + still_standing * hit_prob
-  damage_sequence = rep(c(charge_damage, non_charge_damage), times = c(n_charges, n_elks - n_charges))
-  total_damage = sum(to_hit_sequence * damage_sequence)
+  still_standing   = (1 - hit_and_prone)^cumulative_charge_attempts
+  to_hit_sequence  = (1 - still_standing) * hit_prob_adv + still_standing * hit_prob
+  to_crit_sequence = (1 - still_standing) * crit_prob_adv + still_standing * crit_prob  
+  damage_sequence = rep(c(charge_damage, non_charge_damage), times = c(n_charges, n_animals - n_charges))
+  crit_damage_sequence = rep(
+    c(charge_crit_damage, non_charge_crit_damage), times = c(n_charges, n_animals - n_charges))
+  hit_damage = sum(to_hit_sequence * damage_sequence)
+  crit_damage = sum(to_crit_sequence * crit_damage_sequence)
+  total_damage = hit_damage + crit_damage 
   return(total_damage)
 }
+
--- damage-plot.R
+++ damage-plot.R
@@ -2,12 +2,36 @@ source("conjure-animals.R")
 
 damage_by_AC <- tibble(
   AC      = 6:25,
-  Raptors = sapply(6:25, pack_tactics_damage, atk = 4, dpth = 10, n_animals = 7, n_pack_tactics = 6),
-  Wolves  = sapply(6:25, pack_tactics_damage, atk = 4, dpth = 7, n_animals = 7, n_pack_tactics = 6),  
-  Elks8   = sapply(6:25, elk_damage, STRsave = 4, n_elks = 6, n_charges = 5),
-  Elks6   = sapply(6:25, elk_damage, STRsave = 4, n_elks = 6, n_charges = 3),  
-  Elks4   = sapply(6:25, elk_damage, STRsave = 4, n_elks = 6, n_charges = 2),
-  Elks2   = sapply(6:25, elk_damage, STRsave = 4, n_elks = 6, n_charges = 1))
+  Raptors = sapply(6:25, pack_tactics_damage, atk = 4, dpth = 10, dptc = 6, n_animals = 8, n_pack_tactics = 8),
+  Wolves  = sapply(6:25, pack_tactics_damage, atk = 4, dpth = 7, dptc = 5, n_animals = 8, n_pack_tactics = 8),  
+  Elks8   = sapply(
+    6:25, charge_prone_damage, 
+    STRsave   = 4, n_animals = 8, 
+    n_charges = 8, prone_DC  = 13, 
+    atk_mod   = 5, dmg_mod   = 3,
+    charge_dmg_dice = 10.5, 
+    non_charge_dmg_dice = 5),
+  Elks6   = sapply(
+    6:25, charge_prone_damage, 
+    STRsave = 4, n_animals = 8, 
+    n_charges = 6, prone_DC = 13, 
+    atk_mod = 5, dmg_mod   = 3,
+    charge_dmg_dice = 10.5, 
+    non_charge_dmg_dice = 5),  
+  Elks4   = sapply(
+    6:25, charge_prone_damage, 
+    STRsave = 4, n_animals = 8, 
+    n_charges = 4, prone_DC = 13, 
+    atk_mod = 5, dmg_mod   = 3,
+    charge_dmg_dice = 10.5, 
+    non_charge_dmg_dice = 5),  
+  Elks2   = sapply(
+    6:25, charge_prone_damage, 
+    STRsave = 4, n_animals = 8, 
+    n_charges = 2, prone_DC = 13, 
+    atk_mod = 5, dmg_mod   = 3,
+    charge_dmg_dice = 10.5, 
+    non_charge_dmg_dice = 5))
 
 plot_data <- damage_by_AC %>%
   pivot_longer(
